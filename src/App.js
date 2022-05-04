f7e1a66eca99249f9232a754e60826918d7ff5b3
DikDns <dikdnssocial@gmail.com>
2022-05-04 07:58:20 +0700

    add several links and fix attributes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
       <img className="profile-pic" src={profilePic} alt="DikDns Logo" />
       <ul className="link-list">
         <li className="link-item">
-          <a href="https://www.dikdns.com/" className="link">
+          <a
+            href="https://www.dikdns.com/"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
             <div className="icon-wrapper">
               <i className="icon fa-solid fa-globe"></i>
             </div>
@@ -16,8 +21,101 @@ function App() {
         </li>
         <li className="link-item">
           <a
+            href="https://github.com/DikDns"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i class="icon fa-brands fa-github"></i>
+            </div>
+            Github
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://www.linkedin.com/in/andika-eka-kurnia-4960a5231/"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-linkedin"></i>
+            </div>
+            LinkedIn
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://www.instagram.com/dikdns/"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-instagram"></i>
+            </div>
+            Instagram
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://twitter.com/dikdns_"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-twitter"></i>
+            </div>
+            Twitter
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://www.tiktok.com/@dikdns_"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i class="icon fa-brands fa-tiktok"></i>
+            </div>
+            TikTok
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://www.youtube.com/channel/UCekKhkXSv6RdD9rXkW2ZvDA?sub_confirmation=1"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-youtube"></i>
+            </div>
+            Youtube
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://open.spotify.com/user/kegjluf3vmci3qo4y5g7de8jk"
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="icon-wrapper">
+              <i class="icon fa-brands fa-spotify"></i>
+            </div>
+            Spotify
+          </a>
+        </li>
+        <li className="link-item">
+          <a
             href="https://discordapp.com/users/168393351186481153"
             className="link"
+            target="_blank"
+            rel="noreferrer"
           >
             <div className="icon-wrapper">
               <i class="icon fa-brands fa-discord"></i>
@@ -27,40 +125,15 @@ function App() {
         </li>
         <li className="link-item">
           <a
-            href="https://www.linkedin.com/in/andika-eka-kurnia-4960a5231/"
+            href="mailto:dikdnssocial@gmail.com"
             className="link"
+            target="_blank"
+            rel="noreferrer"
           >
             <div className="icon-wrapper">
-              <i className="icon fa-brands fa-linkedin"></i>
+              <i class="icon fa-solid fa-envelope"></i>
             </div>
-            LinkedIn
-          </a>
-        </li>
-        <li className="link-item">
-          <a href="https://www.instagram.com/dikdns/" className="link">
-            <div className="icon-wrapper">
-              <i className="icon fa-brands fa-instagram"></i>
-            </div>
-            Instagram
-          </a>
-        </li>
-        <li className="link-item">
-          <a href="https://twitter.com/dikdns_" className="link">
-            <div className="icon-wrapper">
-              <i className="icon fa-brands fa-twitter"></i>
-            </div>
-            Twitter
-          </a>
-        </li>
-        <li className="link-item">
-          <a
-            href="https://www.youtube.com/channel/UCekKhkXSv6RdD9rXkW2ZvDA?sub_confirmation=1"
-            className="link"
-          >
-            <div className="icon-wrapper">
-              <i className="icon fa-brands fa-youtube"></i>
-            </div>
-            Youtube
+            Email
           </a>
         </li>
       </ul>
