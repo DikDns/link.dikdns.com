be5dae7c5e06d9920cd9f472048abe559e7855f5
DikDns <dikdnssocial@gmail.com>
2022-05-04 07:27:14 +0700

    fix icon alignment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ function App() {
       <ul className="link-list">
         <li className="link-item">
           <a href="https://www.dikdns.com/" className="link">
-            <i className="icon fa-solid fa-globe"></i> Website
+            <div className="icon-wrapper">
+              <i className="icon fa-solid fa-globe"></i>
+            </div>
+            Website
+          </a>
+        </li>
+        <li className="link-item">
+          <a
+            href="https://discordapp.com/users/168393351186481153"
+            className="link"
+          >
+            <div className="icon-wrapper">
+              <i class="icon fa-brands fa-discord"></i>
+            </div>
+            Discord
           </a>
         </li>
         <li className="link-item">
@@ -16,17 +30,26 @@ function App() {
             href="https://www.linkedin.com/in/andika-eka-kurnia-4960a5231/"
             className="link"
           >
-            <i className="icon fa-brands fa-linkedin"></i> LinkedIn
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-linkedin"></i>
+            </div>
+            LinkedIn
           </a>
         </li>
         <li className="link-item">
           <a href="https://www.instagram.com/dikdns/" className="link">
-            <i className="icon fa-brands fa-instagram"></i> Instagram
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-instagram"></i>
+            </div>
+            Instagram
           </a>
         </li>
         <li className="link-item">
           <a href="https://twitter.com/dikdns_" className="link">
-            <i className="icon fa-brands fa-twitter"></i> Twitter
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-twitter"></i>
+            </div>
+            Twitter
           </a>
         </li>
         <li className="link-item">
@@ -34,7 +57,10 @@ function App() {
             href="https://www.youtube.com/channel/UCekKhkXSv6RdD9rXkW2ZvDA?sub_confirmation=1"
             className="link"
           >
-            <i className="icon fa-brands fa-youtube"></i> Youtube
+            <div className="icon-wrapper">
+              <i className="icon fa-brands fa-youtube"></i>
+            </div>
+            Youtube
           </a>
         </li>
       </ul>
